c4cc251bc55364e876ce63f67abe8e61d7b74f01
nara04040 <nara04040@naver.com>
2023-02-01 11:59:07 +0900

    구현문제7

--- JavaScript구현문제/구현문제7/구현문제7.js
+++ JavaScript구현문제/구현문제7/구현문제7.js
@@ -2,7 +2,7 @@ const questionBtns = document.querySelectorAll(".question-btn");
 
 questionBtns.forEach((button) => {
   button.addEventListener("click", (e) => {
-    const a = e.currentTarget.parentElement.parentElement;
-    a.classList.toggle("show-text");
+    const plusToggleButton = e.currentTarget.parentElement.parentElement;
+    plusToggleButton.classList.toggle("show-text");
   });
 });
