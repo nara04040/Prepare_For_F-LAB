eda3abb00fcef857aa45a962dac4ac819610127c
nara04040 <nara04040@naver.com>
2023-02-10 19:33:28 +0900

    [JS 구현] scroll javascript 구현

--- JavaScript구현문제/Scroll/app.js
+++ JavaScript구현문제/Scroll/app.js
@@ -1,9 +1,11 @@
 const navbar = document.querySelector("nav");
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const navHeight = navbar.getBoundingClientRect().height;
 // fix nav
 const fixNav = () => {
   const scrollHeight = window.pageYOffset;
-  const navHeight = navbar.getBoundingClientRect().height;
-
+  console.log(navHeight);
   if (scrollHeight > navHeight) {
     navbar.classList.add("fixed-nav");
   } else {
@@ -12,3 +14,34 @@ const fixNav = () => {
 };
 
 window.addEventListener("scroll", fixNav);
+
+// fix nav position
+const navLinks = document.querySelectorAll(".scroll-link");
+navLinks.forEach((links) => {
+  links.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    // id는 선언한 id를 뜻한다.
+    const element = document.getElementById(id);
+    // containerHeight : links의 높이
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    // navbar에 fixed-nav가 포함되어 있는지 확인
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    // console.log(fixedNav);
+    // position : links의 상단에서부터 픽셀값 - links의 높이값
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      // navbar에 fixed-nav가 없다면
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+  });
+});
