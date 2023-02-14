5994b63ea14764163e85ccfeca23957803ba4fea
nara04040 <nara04040@naver.com>
2023-02-14 21:40:47 +0900

    [Mentoring] 0214질문리스트

--- JavaScript구현문제/E-commerce/app.js
+++ JavaScript구현문제/E-commerce/app.js
@@ -18,7 +18,15 @@ class Products {
     try {
       let result = await fetch("products.json");
       let data = await result.json();
-      return data;
+
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
     } catch (error) {
       console.log(error);
     }
@@ -26,7 +34,9 @@ class Products {
 }
 
 // display products
-class UI {}
+class UI {
+  displayProducts(products)
+}
 
 // local storage
 class Storage {}
@@ -36,5 +46,5 @@ document.addEventListener("DOMContentLoaded", () => {
   const products = new Products();
 
   // get all products
-  products.getProducts().then((data) => console.log(data));
+  products.getProducts().then((products) => console.log(products));
 });
